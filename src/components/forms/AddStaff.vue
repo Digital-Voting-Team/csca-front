35fcb242f0f99ef0314c45d195a9f0fab5e01431
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2022-12-21 01:58:08 +0200

    Got rid of useless imports

--- src/components/forms/AddStaff.vue
+++ src/components/forms/AddStaff.vue
@@ -16,11 +16,7 @@
 <script>
 import { ref } from "vue";
 import { useAuthUserStore } from "@/stores/auth-user";
-import { AddStaff } from "@/requestsBackend/staff";
-import { StaffRecord } from "@/js/records/staff.record";
 import { AddAddress } from "@/requestsBackend/staff_address";
-import { AddressRecord } from "@/js/records/address.record";
-import { PersonRecord } from "@/js/records/person.record";
 import { AddPerson } from "@/requestsBackend/person";
 import { AddPosition } from "@/requestsBackend/position";
 import { fullStaffRegistration } from "@/requestsBackend/auth";
