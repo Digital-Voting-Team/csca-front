5c13841b153f96a94e24ea0396443c791bba3cc7
Nazarii Savorona <nazar.savorona@gmail.com>
2022-12-22 01:54:39 +0200

    Fix navbar greeting

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
     <div class="container">
       <router-link :to="{ name: 'Home' }" class="navbar-brand">
-        <img class="nav-logo mr-3" src="@/assets/csca_logo.png" alt="Logo"/>
+        <img class="nav-logo mr-3" src="@/assets/csca_logo.png" alt="Logo" />
 
         <span class="d-none d-sm-inline">CSCA</span>
       </router-link>
@@ -11,7 +11,7 @@
         <ul class="navbar-nav d-flex me-2">
 
           <li class="d-flex d-lg-none" v-if="userStorage?.isLoggedIn === true">
-            <a class="nav-link me-2" href="#">Hi there, {{ userStorage?.displayName }}</a>
+            <a class="nav-link me-2 d-none d-sm-block" href="#">Hi there, {{ userStorage?.displayName }}</a>
             <button class="btn btn-danger" @click="handleClick">Logout</button>
           </li>
 
@@ -22,7 +22,7 @@
         </ul>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       </div>
@@ -57,7 +57,7 @@
           </li>
           <li class="nav-item dropdown" v-if="userStorage?.isLoggedIn === true">
             <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-               aria-expanded="false">Views</a>
+              aria-expanded="false">Views</a>
             <ul class="dropdown-menu">
               <li>
                 <router-link class="dropdown-item" :to="{ name: 'StaffView' }">
@@ -77,9 +77,11 @@
             </ul>
           </li>
         </ul>
-        <ul class="m-0">
-          <li class="d-lg-flex d-none d-lg-block" v-if="userStorage?.isLoggedIn === true">
-            <a class="nav-link">Hi there, {{ userStorage?.displayName }}</a>
+
+        <ul class="navbar-nav d-flex me-2">
+
+          <li class="d-none d-lg-flex" v-if="userStorage?.isLoggedIn === true">
+            <a class="nav-link me-2" href="#">Hi there, {{ userStorage?.displayName }}</a>
             <button class="btn btn-danger" @click="handleClick">Logout</button>
           </li>
 
@@ -95,22 +97,22 @@
 
 <script>
 import useLogout from "../composables/useLogout";
-import {useRouter} from "vue-router";
-import {useAuthUserStore} from "@/stores/auth-user";
+import { useRouter } from "vue-router";
+import { useAuthUserStore } from "@/stores/auth-user";
 
 export default {
   setup() {
     const userStorage = useAuthUserStore();
-    const {logout} = useLogout();
+    const { logout } = useLogout();
     const router = useRouter();
 
     const handleClick = async () => {
       await logout(userStorage.username);
       userStorage.isLoggedIn = false;
-      router.push({name: "Login"});
+      router.push({ name: "Login" });
     };
     userStorage.isLoggedIn = false
-    return {userStorage, logout, handleClick, router};
+    return { userStorage, logout, handleClick, router };
   },
 };
 </script>
